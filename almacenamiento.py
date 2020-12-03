78c1e5212a035a20ab55730fb3fb37e3e99af298
lombax <anner.calvo@gmail.com>
2020-12-02 20:00:23 -0600

    TareaCorta 9

--- almacenamiento.py
+++ almacenamiento.py
@@ -1,2 +1,23 @@
 def maximizar(As, D):
-    raise NotImplementedError()
+    AsSorted=sorted(As, key=lambda tup:tup[1])
+    SeqM=[]
+    maxTam=0
+    i=0
+    while i<len(AsSorted):
+        if (maxTam + AsSorted[i][1]<=D):
+            maxTam=maxTam+AsSorted[i][1]
+            SeqM.append(AsSorted[i])
+            i+=1
+        else:
+            break
+
+    return SeqM
+
+
+# def main():
+#     As = [('archivo1', 10), ('archivo2', 5), ('archivo3', 3),
+#               ('archivo4', 8), ('archivo5', 9), ('archivo6', 1)]
+#     D = 10
+#     print(maximizar(As, D))
+
+# main()
